e7bcc80004e70f51bd708ffeea697eadaa82b816
Nikola Rashkov <nikola94rashkov@gmail.com>
2025-02-16 15:44:47 +0200

    Add base eslint, prettier and alias settings

---
+++ client/eslint.config.js
@@ -0,0 +1,87 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const importPlugin = require('eslint-plugin-import');
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    plugins: {
+      import: importPlugin,
+    },
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          pathGroups: [
+            {
+              pattern: '@angular/**',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@models/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@services/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@components/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@pages/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '@routes/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {},
+  },
+);
